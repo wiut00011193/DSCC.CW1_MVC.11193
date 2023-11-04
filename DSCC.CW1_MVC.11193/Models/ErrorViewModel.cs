ae2167bca8d9b4a15984f75504f346f66cbd31fc
wiut00011193 <wiut00011193@gmail.com>
2023-11-04 15:48:28 +0500

    first commit

---
+++ DSCC.CW1_MVC.11193/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DSCC.CW1_MVC._11193.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
