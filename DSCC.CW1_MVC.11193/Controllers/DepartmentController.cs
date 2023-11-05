04caa40ce17520b4d85fc6048ee8227989b8cd44
wiut00011193 <wiut00011193@gmail.com>
2023-11-05 15:33:22 +0500

    added Controllers

---
+++ DSCC.CW1_MVC.11193/Controllers/DepartmentController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DSCC.CW1_MVC._11193.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        // GET: DepartmentController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: DepartmentController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: DepartmentController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: DepartmentController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: DepartmentController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: DepartmentController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: DepartmentController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: DepartmentController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- DSCC.CW1_MVC.11193/Models/Department.cs
+++ DSCC.CW1_MVC.11193/Models/Department.cs
@@ -1,6 +1,12 @@
-﻿namespace DSCC.CW1_MVC._11193.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DSCC.CW1_MVC._11193.Models
 {
     public class Department
     {
+        public int ID { get; set; }
+
+        [Display(Name = "Department")]
+        public string Name { get; set; }
     }
 }
