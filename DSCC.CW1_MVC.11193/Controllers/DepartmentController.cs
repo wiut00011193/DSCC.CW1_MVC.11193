633376aa5a0803b75dada1d0462872898110f24a
wiut00011193 <wiut00011193@gmail.com>
2023-11-05 18:57:54 +0500

    added Department List View

--- DSCC.CW1_MVC.11193/Controllers/DepartmentController.cs
+++ DSCC.CW1_MVC.11193/Controllers/DepartmentController.cs
@@ -1,14 +1,55 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using DSCC.CW1_MVC._11193.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
 
 namespace DSCC.CW1_MVC._11193.Controllers
 {
     public class DepartmentController : Controller
     {
-        // GET: DepartmentController
-        public ActionResult Index()
+        // The Definition of Base URL
+        public const string baseUrl = "http://localhost:5116/";
+        Uri ClientBaseAddress = new Uri(baseUrl);
+        HttpClient clnt;
+
+        // Constructor for initiating request to the given base URL publicly
+        public DepartmentController()
         {
-            return View();
+            clnt = new HttpClient();
+            clnt.BaseAddress = ClientBaseAddress;
+        }
+
+        public void HeaderClearing()
+        {
+            // Clearing default headers
+            clnt.DefaultRequestHeaders.Clear();
+
+            // Define the request type of the data
+            clnt.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        // GET: Department
+        public async Task<ActionResult> Index()
+        {
+            // Creating the list of new Departments list
+            List<Department> DepartmentInfo = new List<Department>();
+
+            HeaderClearing();
+
+            // Sending Request to the find web api Rest service resources using HTTPClient
+            HttpResponseMessage httpResponseMessage = await clnt.GetAsync("api/Department");
+
+            // If the request is success
+            if (httpResponseMessage.IsSuccessStatusCode)
+            {
+                // storing the web api data into model that was predefined prior
+                var responseMessage = httpResponseMessage.Content.ReadAsStringAsync().Result;
+
+                DepartmentInfo = JsonConvert.DeserializeObject<List<Department>>(responseMessage);
+            }
+
+            return View(DepartmentInfo);
         }
 
         // GET: DepartmentController/Details/5
